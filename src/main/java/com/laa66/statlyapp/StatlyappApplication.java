23c3e7235f100954bc7bd5a8e95c70b025265b80
laa66 <max.sclz94@gmail.com>
2023-02-20 19:46:46 +0100

    Project created with Spring Initializr

---
+++ src/main/java/com/laa66/statlyapp/StatlyappApplication.java
@@ -0,0 +1,13 @@
+package com.laa66.statlyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatlyappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatlyappApplication.class, args);
+	}
+
+}
