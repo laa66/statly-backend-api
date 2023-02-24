0336ddbf014ac3ff0b803fd2f0ab41bd3e0f92cd
laa66 <max.sclz94@gmail.com>
2023-02-24 14:31:58 +0100

    Implemented app Controller class

--- src/main/java/com/laa66/statlyapp/controller/AppController.java
+++ src/main/java/com/laa66/statlyapp/controller/AppController.java
@@ -4,28 +4,49 @@ import com.laa66.statlyapp.constants.SpotifyAPI;
 import com.laa66.statlyapp.model.exchange.*;
 import com.laa66.statlyapp.service.SpotifyApiService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api")
 public class AppController {
 
     @Autowired
     private SpotifyApiService spotifyApiService;
 
-    @GetMapping("/topTracks")
-    public SpotifyResponseTopTracks tracks() {
-        return spotifyApiService.getTopTracks(SpotifyAPI.TOP_TRACKS_SHORT);
+    @GetMapping("/top/tracks")
+    public SpotifyResponseTopTracks tracks(@RequestParam("range") String range) {
+        SpotifyResponseTopTracks tracks;
+        switch (range) {
+            case "short" -> tracks = spotifyApiService.getTopTracks(SpotifyAPI.TOP_TRACKS_SHORT);
+            case "medium" -> tracks = spotifyApiService.getTopTracks(SpotifyAPI.TOP_TRACKS_MEDIUM);
+            case "long" -> tracks = spotifyApiService.getTopTracks(SpotifyAPI.TOP_TRACKS_LONG);
+            default -> throw new RuntimeException();
+        }
+        return tracks;
     }
 
-    @GetMapping("/topArtists")
-    public SpotifyResponseTopArtists artists() {
-        return spotifyApiService.getTopArtists(SpotifyAPI.TOP_ARTISTS_SHORT);
+    @GetMapping("/top/artists")
+    public SpotifyResponseTopArtists artists(@RequestParam("range") String range) {
+        SpotifyResponseTopArtists artists;
+        switch (range) {
+            case "short" -> artists = spotifyApiService.getTopArtists(SpotifyAPI.TOP_ARTISTS_SHORT);
+            case "medium" -> artists = spotifyApiService.getTopArtists(SpotifyAPI.TOP_ARTISTS_MEDIUM);
+            case "long" -> artists = spotifyApiService.getTopArtists(SpotifyAPI.TOP_ARTISTS_LONG);
+            default -> throw new RuntimeException();
+        }
+        return artists;
     }
 
-    @GetMapping("/topGenres")
-    public SpotifyResponseTopGenres genres() {
-        return spotifyApiService.getTopGenres(SpotifyAPI.TOP_ARTISTS_SHORT);
+    @GetMapping("/top/genres")
+    public SpotifyResponseTopGenres genres(@RequestParam("range") String range) {
+        SpotifyResponseTopGenres genres;
+        switch (range) {
+            case "short" -> genres = spotifyApiService.getTopGenres(SpotifyAPI.TOP_ARTISTS_SHORT);
+            case "medium" -> genres = spotifyApiService.getTopGenres(SpotifyAPI.TOP_ARTISTS_MEDIUM);
+            case "long" -> genres = spotifyApiService.getTopGenres(SpotifyAPI.TOP_ARTISTS_LONG);
+            default -> throw new RuntimeException();
+        }
+        return genres;
     }
 
     @GetMapping("/recently")
@@ -34,13 +55,27 @@ public class AppController {
     }
 
     @GetMapping("/score")
-    public SpotifyResponseMainstreamScore mainstreamScore() {
-        return spotifyApiService.getMainstreamScore(SpotifyAPI.TOP_ARTISTS_SHORT);
+    public SpotifyResponseMainstreamScore mainstreamScore(@RequestParam("range") String range) {
+        SpotifyResponseMainstreamScore score;
+        switch (range) {
+            case "short" -> score = spotifyApiService.getMainstreamScore(SpotifyAPI.TOP_ARTISTS_SHORT);
+            case "medium" -> score = spotifyApiService.getMainstreamScore(SpotifyAPI.TOP_ARTISTS_MEDIUM);
+            case "long" -> score = spotifyApiService.getMainstreamScore(SpotifyAPI.TOP_ARTISTS_LONG);
+            default -> throw new RuntimeException();
+        }
+        return score;
     }
 
-    @GetMapping("/create")
-    public String createPlaylist() {
-        return spotifyApiService.postTopTracksPlaylist(SpotifyAPI.TOP_TRACKS_SHORT);
+    @PostMapping("/playlist/create")
+    public String createPlaylist(@RequestParam("range") String range) {
+        String response;
+        switch (range) {
+            case "short" -> response = spotifyApiService.postTopTracksPlaylist(SpotifyAPI.TOP_TRACKS_SHORT);
+            case "medium" -> response = spotifyApiService.postTopTracksPlaylist(SpotifyAPI.TOP_TRACKS_MEDIUM);
+            case "long" -> response = spotifyApiService.postTopTracksPlaylist(SpotifyAPI.TOP_TRACKS_LONG);
+            default -> throw new RuntimeException();
+        }
+        return response;
     }
 
 }
