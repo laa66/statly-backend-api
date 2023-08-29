516673c9263a57b0f0bfa40fda32b34b2c2898a6
laa66 <max.sclz94@gmail.com>
2023-08-29 19:43:51 +0200

    Updated README.md, modified app properties and docker-compose.yaml

--- src/test/java/com/laa66/statlyapp/controller/LocationDataControllerUnitTest.java
+++ src/test/java/com/laa66/statlyapp/controller/LocationDataControllerUnitTest.java
@@ -7,6 +7,7 @@ import com.laa66.statlyapp.model.OAuth2UserWrapper;
 import com.laa66.statlyapp.repository.SpotifyTokenRepository;
 import com.laa66.statlyapp.service.LocationService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
