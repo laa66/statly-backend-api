6daa19f6c9ab261fed78ce19799af80dc266f384
laa66 <max.sclz94@gmail.com>
2023-11-30 22:48:29 +0100

    Fixed cron in scheduled method saveCache in CacheTask

--- src/main/java/com/laa66/statlyapp/task/CacheTask.java
+++ src/main/java/com/laa66/statlyapp/task/CacheTask.java
@@ -22,7 +22,7 @@ public class CacheTask {
     private final CacheManager cacheManager;
     private final StatsService statsService;
 
-    @Scheduled(cron = "0 06 22 * * *")
+    @Scheduled(cron = "0 59 21 * * *")
     public void saveCache() {
         Optional.ofNullable((CaffeineCache) cacheManager.getCache("api"))
                 .ifPresentOrElse(caffeineCache -> {
